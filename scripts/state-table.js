9a196c1f50bb2c42ba09ed145300312e34891fb3
ZorroZ77 <ozerhusen@gmail.com>
2023-05-13 09:17:58 -0500

    create state dropdown list

--- scripts/state-table.js
+++ scripts/state-table.js
@@ -1,13 +1,15 @@
 // fetch state politicians data
-$(function () {
+function fetchPoliticiansData(state) {
     console.log("Requesting politicians data...");
-    const state = getUrlParameter("state").substring(3);
 
     $.ajax({
         url: "http://localhost/dashboard/devcodes/CryptoPols/db/fetch_politicians.php", //http://cryptopols.com/db/fetch_politicians.php //http://localhost/dashboard/devcodes/CryptoPols/db/fetch_politicians.php
         dataType: "json",
         data: { state: state },
         success: function (politicians) {
+            // Clear existing data
+            $('#politician-table tbody').empty();
+
             console.log("Politicians Data:", politicians);
             const baseURL = 'https://www.govtrack.us/static/legislator-photos/';
 
@@ -29,6 +31,13 @@ $(function () {
                 $('#politician-table tbody').append(row);
             });
 
+            $("#politician-table").trigger("update");
+
+            // Wait for the table to be processed, then sort:
+            setTimeout(function () {
+                $("#politician-table").trigger("sorton", [[[2, 1], [3, 0]]]);
+            }, 0);
+
             console.log("tablesorter plugin:", $.tablesorter);
             initTablesorter();
         },
@@ -38,12 +47,12 @@ $(function () {
             console.log("Response:", jqXHR.responseText);
         }
     });
-});
-
-function getUrlParameter(name) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(name);
 }
+
+$(function () {
+    const state = getUrlParameter("state").substring(3);
+    fetchPoliticiansData(state);
+});
 
 // set table sorting with default settings
 function initTablesorter() {
@@ -115,3 +124,105 @@ function initTablesorter() {
    };
    */
 }
+
+
+
+
+
+$(document).ready(function () {
+    const states = [
+        { name: 'Alabama', id: 'AL' },
+        { name: 'Alaska', id: 'AK' },
+        { name: 'Arizona', id: 'AZ' },
+        { name: 'Arkansas', id: 'AR' },
+        { name: 'California', id: 'CA' },
+        { name: 'Colorado', id: 'CO' },
+        { name: 'Connecticut', id: 'CT' },
+        { name: 'Delaware', id: 'DE' },
+        { name: 'Florida', id: 'FL' },
+        { name: 'Georgia', id: 'GA' },
+        { name: 'Hawaii', id: 'HI' },
+        { name: 'Idaho', id: 'ID' },
+        { name: 'Illinois', id: 'IL' },
+        { name: 'Indiana', id: 'IN' },
+        { name: 'Iowa', id: 'IA' },
+        { name: 'Kansas', id: 'KS' },
+        { name: 'Kentucky', id: 'KY' },
+        { name: 'Louisiana', id: 'LA' },
+        { name: 'Maine', id: 'ME' },
+        { name: 'Maryland', id: 'MD' },
+        { name: 'Massachusetts', id: 'MA' },
+        { name: 'Michigan', id: 'MI' },
+        { name: 'Minnesota', id: 'MN' },
+        { name: 'Mississippi', id: 'MS' },
+        { name: 'Missouri', id: 'MO' },
+        { name: 'Montana', id: 'MT' },
+        { name: 'Nebraska', id: 'NE' },
+        { name: 'Nevada', id: 'NV' },
+        { name: 'New Hampshire', id: 'NH' },
+        { name: 'New Jersey', id: 'NJ' },
+        { name: 'New Mexico', id: 'NM' },
+        { name: 'New York', id: 'NY' },
+        { name: 'North Carolina', id: 'NC' },
+        { name: 'North Dakota', id: 'ND' },
+        { name: 'Ohio', id: 'OH' },
+        { name: 'Oklahoma', id: 'OK' },
+        { name: 'Oregon', id: 'OR' },
+        { name: 'Pennsylvania', id: 'PA' },
+        { name: 'Rhode Island', id: 'RI' },
+        { name: 'South Carolina', id: 'SC' },
+        { name: 'South Dakota', id: 'SD' },
+        { name: 'Tennessee', id: 'TN' },
+        { name: 'Texas', id: 'TX' },
+        { name: 'Utah', id: 'UT' },
+        { name: 'Vermont', id: 'VT' },
+        { name: 'Virginia', id: 'VA' },
+        { name: 'Washington', id: 'WA' },
+        { name: 'West Virginia', id: 'WV' },
+        { name: 'Wisconsin', id: 'WI' },
+        { name: 'Wyoming', id: 'WY' }
+    ];
+
+    const $stateSelect = $('#state-select');
+
+    states.forEach(state => {
+        $stateSelect.append(new Option(state.name, state.id));
+    });
+
+    $stateSelect.select2({      
+        allowClear: true
+    });
+
+    $stateSelect.on('select2:select', function (e) {
+        var stateId = $(this).val();
+        var stateName = $(this).find('option:selected').text();
+        if (stateId) {
+            fetchPoliticiansData(stateId);
+            $('#state-name').text(stateName);
+
+            // Fetch the state name (and sentiment)
+            fetchStateName(stateId);
+        }
+    });
+
+
+
+    $('select').select2({
+        width: '18rem',
+    });
+
+    const urlStateId = getQueryParam("state");
+    if (urlStateId) {
+        const stateAbbr = urlStateId.substring(3);
+        fetchPoliticiansData(stateAbbr);
+        fetchStateName(stateAbbr);
+    } else {
+        // If no state parameter in the URL, get the selected state from the dropdown
+        const selectedStateId = $('#state-select').val();
+        if (selectedStateId) {
+            fetchPoliticiansData(selectedStateId);
+            fetchStateName(selectedStateId);
+        }
+    }
+
+});
